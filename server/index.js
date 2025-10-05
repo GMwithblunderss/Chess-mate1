842889227368c2d1aed00376e051f374f7c8ab44
Daksh <chhabradaksh05@gmail.com>
2025-10-05 21:10:55 +0530

    hardened cors

--- server/index.js
+++ server/index.js
@@ -39,9 +39,35 @@
     next();
     });
 
-    app.use(cors());
-    app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+const allowedOrigin = process.env.APP_API_URL && process.env.APP_API_URL.replace(/\/+$/, '');
+
+app.use((req, res, next) => {
+  if (!allowedOrigin) {
+    return cors()(req, res, next);
+  }
+
+  const origin = req.headers.origin && req.headers.origin.replace(/\/+$/, '');
+
+  if (!origin || origin === allowedOrigin) {
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    if (req.method === 'OPTIONS') {
+      return res.status(204).end();
+    }
+    return next();
+  }
+
+  if (req.method === 'OPTIONS') {
+    return res.status(403).json({ error: 'CORS: Origin not allowed' });
+  }
+  return res.status(403).json({ error: 'CORS: Origin not allowed' });
+});
+
+    app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(
   [
     '/username',
