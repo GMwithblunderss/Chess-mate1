10e238c4d5f3af45ba67c820f22e5554fed85a35
GMwithblunderss <gmwithblunderss@gmail.com>
2025-10-25 00:27:38 +0530

    Fix static file serving and route handling

--- server/index.js
+++ server/index.js
@@ -732,14 +732,12 @@ app.post("/gradePvMove", async (req, res) => {
   );
 }
 else{
-    app.use(express.static(path.join(__dirname, '../build')));   
-);
-
+    app.use(express.static(path.join(__dirname, '../build')));
 }
 
-    app.get('/{*splat}', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
-    }
+    });
 
     app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
