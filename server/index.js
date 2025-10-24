83d7ad0eaca6854070b7a2ae3547b3a523a6bc68
GMwithblunderss <gmwithblunderss@gmail.com>
2025-10-25 00:58:57 +0530

    Refactor server index.js for production handling

--- server/index.js
+++ server/index.js
@@ -721,24 +721,11 @@ app.post("/gradePvMove", async (req, res) => {
     }); */
 
 
-    if (process.env.NODE_ENV !== 'production') {
-  app.use(
-    '/',
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-      ws: true,
-    })
-  );
-}
-else{
-    app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
-        app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../build', 'index.html'));
-    });
-}
-
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 
     app.listen(PORT, () => {
